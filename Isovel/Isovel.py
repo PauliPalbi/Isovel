4d58c83b160bd20f37af46b240d1ece0d4c7c165
Paulina Palma <paulinapalmab@hotmail.com>
2020-06-26 01:52:08 -0400

    closer for it to work

--- Isovel/Isovel.py
+++ Isovel/Isovel.py
@@ -1,30 +1,21 @@
-# This to start documentation
+# Class that calculate the velocity of the gas 
 import numpy as np
 import astropy.constants as const
+import astropy.units as u
 from scipy.ndimage.interpolation import rotate
 from scipy import optimize
-#'PA': 325.03970248937446,
-# 'self.mstar': 1.1835185573440012,
-# 'inc': 46.88375837648006,
-# 
-# 'dist': 157.68621074959017,
-# 'vlsr': 4744.619070925755,
-#
-# 'z0': 0.24717562173400856,
-# 'psi': 1.1814702194561688
+import matplotlib.pyplot as plt
+from astropy.io import fits
 
 class Isovel:
     def __init__(self, mstar, pa, inc, z0, psi, vlsr, vels_lev, 
-                R_max=300, v_max=5, nx=300, ny=300, southcloser=True, pix_to_au =1):
+                R_max=300, v_max=5, nx=300, ny=300, pix_to_au =1, border=550):
 
-        M_sun = const.M_sun
+        M_sun = const.M_sun.to(u.g)
 
         self.mstar = mstar * M_sun.value
 
-        if southcloser:
-            self.inc= np.radians(180+inc) # south is closer
-        else:
-            self.inc = np.radians(360-inc) # north is closer
+        self.inc= np.radians(inc) # south is closer
 
         self.pa = pa
         #pa = 90-85.4526 #check later for correct formula
@@ -43,207 +34,76 @@ class Isovel:
         self.nx = nx
         self.ny = ny
         self.pix_to_au = pix_to_au 
+        self.border = border
 
 
-    def Keplerian_rotation(self, rr_cm, theta):
-        """
-        multiplication
+    def isovel(self):
+            # projected coordinates (should be xprime, yprime)
+        G = const.G.value*1000
 
-        Args: 
-            
-        Returns:
-        
-            float: multiplication
-        """
-        G = const.G
-        vel = np.sqrt(G.value * self.mstar / rr_cm) 
-        vel *= np.cos(theta) * np.sin(np.radians(self.inc)) # km/s
-        
-        # rotate
-        pa = np.radians(self.pa)
-        vel = rotate(vel, pa)
-        return vel
-
-    def create_shape(self):
-        """
-        """
         xp = np.linspace(-self.R_max, self.R_max, self.nx)
         yp = np.linspace(-self.R_max, self.R_max, self.ny)
         xxp, yyp = np.meshgrid(xp, yp)
-        return xxp, yyp
 
-
-    def deproject_coordinates(self, x, y):
-        """
-        multiplication
-
-        Args: 
-            
-        Returns:
-        
-            float: multiplication
-        """
-        inc = np.radians(self.inc)
-        xx = x
-        yy = y/np.cos(inc)
-        return xx, yy
-
-
-    def cilindrical_coords(self, xx, yy, zz, Dim_3 = False):
-        if not Dim_3:
-            rr = np.sqrt(xx**2 + yy**2)*self.pix_to_au # au
-        else:
-            rr = np.sqrt(xx**2 + yy**2 + zz**2)*self.pix_to_au  # au
-        au = 1.496e8 # from au to km
-        rr_km = rr*au # km
+        xx = xxp 
+        yy = yyp/np.cos(self.inc)
+        rr = np.sqrt(xx**2+yy**2)*pix_to_au # au
+        au = 14960000000000
+        rr_cm = rr*au # cm
         theta = np.arctan2(yy,xx)
-        return rr_km, theta
 
+            # projected line of sight velocity at x',y'
+        vel_thin = np.sqrt(G*self.mstar/rr_cm)*np.sin(self.inc)*np.cos(theta) # cm/s
+        vel_thin /= 1e5 # km/s
+        
+            # rotate
+        vel_thin = rotate(vel_thin,self.pa, reshape=False)
 
-    def function_int(self, t, ixp, iyp):
-        """
-        we assume flared is a cone
+        seci = 1/np.cos(self.inc)
+        def f(t,ixp,iyp):
+            return (np.cos(2*self.inc) + np.cos(2*self.phi))*t**2 - (2*np.sin(self.phi)**2*2*iyp*np.tan(self.inc))*t - (2*np.sin(self.phi)**2*(ixp**2 + iyp**2*seci**2 ))
 
-        intersection of line with cone
-        # Flared disk -> cone
+        tt_near = np.zeros(yyp.shape) # near side of the disk
+        tt_far = np.zeros(yyp.shape) # far side of the disk
 
-        # intersection of line with cone:
-        # yp = (y - t*sin(i))*cosi
-        # zp = t*cos(i)
-
-        # where t are the solution of a line intersecting a cone
-        # t**2 * (np.cos(2*i) + np.cos(2*phi)) - 2*np.sin(phi)**2 * (xxp**2 + yyp**2*np.sec(i)**2 + 2*t*yyp*np.tan(i)) = 0
-        # The positive and negative roots of this equation correspond tothe front and back halves
-        # of the double cone, respectively
-
-        # solve for t
-
-        Args: 
-            t (array):are the solution of a line intersecting a cone
-            ixp ():
-            iyp ():
-        Returns:
-            array:
-
-        """
-        inc = np.radians(self.inc)
-        seci = 1/np.cos(inc)
-
-        a = (np.cos( 2 * inc) + np.cos( 2 * self.phi)) * t**2 
-        b = - (2 * np.sin(self.phi)**2 * 2 * iyp * np.tan(inc)) * t 
-        c = - (2 * np.sin(self.phi)**2 * (ixp**2 + iyp**2 * seci**2 ))
-        res = a+b+c
-        return res
-
-    def surface_projection(self, xxp, yyp):
-        """
-        """
-        #vel_front
-        tt_front = np.zeros(yyp.shape) # front side of the disk
-        #vel_back
-        tt_back = np.zeros(yyp.shape) # back side of the disk
         for ix in range(self.nx):
             for iy in range(self.ny):
-                sol = optimize.root(function_int, [0.], args=(xxp[ix,iy],yyp[ix,iy]), method='lm')
-                if self.inc<180:
-                    tt_front[ix,iy] = np.abs(sol.x) # au
-                    tt_back[ix,iy] = -np.abs(sol.x)
+                sol = optimize.root(f, [0.], args=(xxp[ix,iy],yyp[ix,iy]), method='lm')
+                if self.inc < np.pi:
+                    tt_near[ix,iy] = np.abs(sol.x) # au
+                    tt_far[ix,iy] = -np.abs(sol.x)
                 else:
-                    tt_back[ix,iy] = np.abs(sol.x) # au
-                    tt_front[ix,iy] = -np.abs(sol.x)
-        return tt_front, tt_back
+                    tt_far[ix,iy] = np.abs(sol.x) # au
+                    tt_near[ix,iy] = -np.abs(sol.x)
+
+        yy_near = yyp/np.cos(self.inc) + tt_near*np.sin(self.inc)
+        zz_near = tt_near*np.cos(self.inc)
+        rr_near = np.sqrt(xx**2+yy_near**2+zz_near**2)*pix_to_au # au
+        #rr_pos = np.sqrt(xx**2+yy_pos**2) # au
+        rr_near_cm = rr_near*au # cm
+        theta_near = np.arctan2(yy_near,xx)
+
+        yy_far = yyp/np.cos(self.inc) + tt_far*np.sin(self.inc)
+        zz_far = tt_far*np.cos(self.inc)
+        rr_far = np.sqrt(xx**2+yy_far**2+zz_far**2)*pix_to_au # au
+        #rr_neg = np.sqrt(xx**2+yy_neg**2) # au
+        rr_far_cm = rr_far*au # cm
+        theta_far = np.arctan2(yy_far,xx)
+
+        vel_near = np.sqrt(G*mstar/rr_near_cm)*np.sin(self.inc)*np.cos(theta_near)/1e5 # km/s
+        vel_far = np.sqrt(G*mstar/rr_far_cm)*np.sin(self.inc)*np.cos(theta_far)/1e5 # km/s
+
+            # rotate
+        vel_near = rotate(vel_near,self.pa, reshape=False)
+        vel_far = rotate(vel_far,self.pa, reshape=False)
 
 
-    def calculate_vel(self, border=550):
-        """
-        """
+        mask_contour_near = rotate(rr_near, self.pa, reshape=False)[:][:]>self.border
+        mask_contour_far= rotate(rr_far, self.pa, reshape=False)[:][:]>self.border
+        mask_contour_thin = rotate(rr, self.pa, reshape=False)[:][:]>self.border
         
-        xxp, yyp = self.create_shape()
-        x_dep, y_dep = self.deproject_coordinates(xxp, yyp)
-
-        z=np.array([])
-        rr, theta = self.cilindrical_coords(x_dep, y_dep, z) #km and rad
-
-        vel_thin = self.Keplerian_rotation(rr, theta)
-
-        tt_front, tt_back = self.surface_projection(x_dep, y_dep)
-
-        inc = np.radians(self.inc)
-
-        yy_front = y_dep + tt_front*np.sin(inc)
-        zz_front = tt_front*np.cos(inc)
-        print(zz_front)
-        rr_front, theta_front = self.cilindrical_coords(x_dep, yy_front, zz_front, Dim_3=True)
-        
-        yy_back = y_dep + tt_back*np.sin(inc)
-        zz_back = tt_back*np.cos(inc)
-        rr_back, theta_back = self.cilindrical_coords(x_dep, yy_back, zz=zz_back, Dim_3=True)
-        
-
-        vel_front = self.Keplerian_rotation(rr_front, theta_front)
-        vel_back = self.Keplerian_rotation(rr_back, theta_back)
-        '''
-        pa = np.radians(self.pa)
-        mask_contour_front = rotate(rr_front, pa, reshape=False)[:][:]>border
-        mask_contour_back= rotate(rr_back, pa, reshape=False)[:][:]>border
-        mask_contour_thin = rotate(rr, pa, reshape=False)[:][:]>border
-
-        vel_front = np.ma.array(vel_front, mask=mask_contour_front)
-        vel_back = np.ma.array(vel_back, mask=mask_contour_back)
+        vel_near = np.ma.array(vel_near, mask=mask_contour_near)
+        vel_far = np.ma.array(vel_far, mask=mask_contour_far)
         vel_thin = np.ma.array(vel_thin, mask=mask_contour_thin)
-        '''
-        return vel_thin, vel_front, vel_back
 
-
-
-
-    def power_z_function(self, r):
-        """
-        power law of surface shape, not used for now
-
-
-        """
-        z = self.z0 * np.power(r, self.psi)
-        return z
-
-
-
-    '''
-    def plot_velocities(self):
-            # Plot
-        fig, (ax1,ax2,ax3,ax4) = pl.subplots(1,4,figsize=(10,10))
-        ext=[-Rmax,Rmax,-Rmax,Rmax]
-
-        im1 = ax1.imshow(vel_thin,extent=ext,origin='lower',vmin=-vmax,vmax=vmax)
-        ax1.contour(vel_thin,vels_lev,extent=ext,origin='lower',colors='white',linestyles='solid',linewidths=2)
-
-        im2 = ax2.imshow(vel_front,extent=ext,origin='lower',vmin=-vmax,vmax=vmax)
-        ax2.contour(vel_front,vels_lev,extent=ext,origin='lower',colors='white',linestyles='dashed',linewidths=2)
-
-        im3 = ax3.imshow(vel_back,extent=ext,origin='lower',vmin=-vmax,vmax=vmax)
-        ax3.contour(vel_back,vels_lev,extent=ext,origin='lower',colors='white',linestyles='dotted',linewidths=2)
-
-        cols = ['red','red','red','blue','blue','blue']
-        im4 = ax4.imshow(vel_thin*0,extent=ext,origin='lower',cmap='gray_r')
-        #ax4.contour(vel_thin,[-1.05,1.05],extent=ext,origin='lower',colors=cols,linestyles='solid',linewidths=2)
-        #ax4.contour(vel_front,[-1.05,1.05],extent=ext,origin='lower',colors=cols,linestyles='dashed',linewidths=2)
-        #ax4.contour(vel_back,[-1.05,1.05],extent=ext,origin='lower',colors=cols,linestyles='dotted',linewidths=2)
-
-        ax4.contour(vel_front,vels_lev,extent=ext,origin='lower',colors=cols,linestyles='dashed',linewidths=2)
-        ax4.contour(vel_back,vels_lev,extent=ext,origin='lower',colors=cols,linestyles='dotted',linewidths=2)
-
-        for ax in ax1,ax2,ax3,ax4:
-            ax.set_xlim([-Rmax,Rmax])
-            ax.set_ylim([-Rmax,Rmax])
-
-
-        pl.show()
-    '''
-
-
-mstar, pa, inc, z0, psi, vlsr =(1.2047920150052118, 325.15077530944075, 46.41703391829195, 0.2494024079038776, 1.211576566441788, 4.741979863714648)
-vels_lev = np.array([3,3.7,4.4,5.1,5.8,6.5])
-
-Isovel_disk = Isovel(mstar, pa, inc, z0, psi, vlsr, vels_lev)
-vel_thin, vel_front, vel_back = Isovel_disk.calculate_vel()
-print(vel_thin, vel_front, vel_back)
+        return vel_near, vel_far, vel_thin
