f6288ab742eadac852b06d62c0e5a310901f26c9
Paulina Palma <paulinapalmab@hotmail.com>
2020-06-26 11:02:08 -0400

    Some more

--- Isovel/Driver.py
+++ Isovel/Driver.py
@@ -11,29 +11,12 @@ from isovel import Isovel
 #The way of using this and to do a plot
 #the values were obtain with fit_shape
 ###########################
-mstar, pa, inc, z0, psi, vlsr =(1.2047920150052118, 325.15077530944075, 46.41703391829195, 0.2494024079038776, 1.211576566441788, 4.741979863714648)
-pa = 90-pa-180
-border = 550.
-
-pix_size = 2.9e-3
-par=6.341708607533389 #mas
-d = 1000/par #pc
-pix_to_au = (pix_size*d)
-
-Isovel_disk = Isovel(mstar, pa, inc, z0, psi, vlsr, pix_to_au=pix_to_au)
-vel_thin, vel_front, vel_back  = Isovel_disk.velocities()
-
-print(vel_thin, vel_front, vel_back)
-
-
-sizefig = (10., 6.)
-
-channels= np.arange(-7.99991, 16.1501, 0.35)
 
 path = '/Volumes/Transcend/J1615_2020/DataWork/Results/CO/'
 name_12CO = path+'J1615_12CO_im_uv006'
 fits_image = name_12CO +'.fits'
 
+channels= np.arange(-7.99991, 16.1501, 0.35)
 
 image_1 = fits.open(fits_image)
 data_1 = image_1[0].data[0][0]
@@ -41,7 +24,21 @@ imsize_1 = len(data_1[0])
 Rmax = imsize_1/2. # pix
 header = image_1[0].header
 pixscales=np.abs(3600*header['CDELT2'])
-arcs = imsize_1/2.*pixscales
+arcs = Rmax * pixscales
+arcs = [arcs,-arcs,-arcs,arcs]
+
+
+mstar, pa, inc, z0, psi, vlsr =(1.2047920150052118, 325.15077530944075, 46.41703391829195, 0.2494024079038776, 1.211576566441788, 4.741979863714648)
+pa = 90-pa-180
+border = 550.
+par=6.341708607533389 #mas
+d = 1000/par #pc
+pix_to_au = (pixscales*d)
+
+Isovel_disk = Isovel(mstar, pa, inc, z0, psi, vlsr, pix_to_au, border = border)
+vel_thin, vel_front, vel_back  = Isovel_disk.velocities()
+
+print(vel_thin, vel_front, vel_back)
 
 fig, axs = plt.subplots(3, 3, sharey=True, figsize=(16, 16))
 for i in range(32,41):
@@ -60,11 +57,9 @@ for i in range(32,41):
     image_12mm = np.zeros([imsize_1, imsize_1])
     image_12mm[:][:] = data_1[:][:]
     
-    vels_lev = np.array([channels[i]])-vlsr
-    axs[a,b].imshow(image_12mm,origin='lower', cmap='gist_heat', vmin=0, vmax=0.03, extent=[arcs,-arcs,-arcs,arcs])#, norm=colors.PowerNorm(gamma=gval),vmin=rms,vmax=maxi)
-    axs[a,b].contour(vel_front,vels_lev,origin='lower',colors='cyan',linestyles='dashed',linewidths=2.1, extent=[arcs,-arcs,-arcs,arcs], corner_mask=True)
-    axs[a,b].contour(vel_back,vels_lev,origin='lower',colors='cyan',linestyles='dotted',linewidths=1.2, extent=[arcs,-arcs,-arcs,arcs], corner_mask=True)
-    #axs[a,b].set_xlim(3, -3)
-    #axs[a,b].set_ylim(-3, 3)
+    vels_lev = (np.array([channels[i]])-vlsr)
+    axs[a,b].imshow(image_12mm,origin='lower', cmap='gist_heat', extent=arcs)
+    axs[a,b].contour(vel_front,vels_lev,origin='lower',colors='cyan',linestyles='dashed',linewidths=2.1, extent=arcs, corner_mask=True)
+    axs[a,b].contour(vel_back,vels_lev,origin='lower',colors='cyan',linestyles='dotted',linewidths=1.2, extent=arcs, corner_mask=True)
 
 fig.savefig('J1615_isovelocities.png',dpi=600)
--- Isovel/Isovel.py
+++ Isovel/Isovel.py
@@ -67,7 +67,7 @@ class Isovel:
         """
             # projected coordinates (should be xprime, yprime)
         G = const.G.value*1000
-        au = 14960000000000
+        au = 1.496e13
 
         xp = np.linspace(-self.R_max, self.R_max, self.nx)
         yp = np.linspace(-self.R_max, self.R_max, self.ny)
@@ -75,8 +75,7 @@ class Isovel:
 
         xx = xxp 
         yy = yyp/np.cos(self.inc)
-        rr = np.sqrt(xx**2+yy**2) * self.pix_to_au # au
-        rr_cm = rr*au # cm
+        rr_cm = np.sqrt(xx**2+yy**2) * self.pix_to_au *au # cm
         theta = np.arctan2(yy,xx)
 
             # projected line of sight velocity at x',y'
@@ -84,7 +83,7 @@ class Isovel:
         vel_thin /= 1e5 # km/s
         
             # rotate
-        vel_thin = rotate(vel_thin,self.pa, reshape=False)
+        vel_thin = rotate(vel_thin,-self.pa, reshape=False)
 
         seci = 1/np.cos(self.inc)
         def f(t,ixp,iyp):
@@ -96,23 +95,22 @@ class Isovel:
         for ix in range(self.nx):
             for iy in range(self.ny):
                 sol = optimize.root(f, [0.], args=(xxp[ix,iy],yyp[ix,iy]), method='lm')
-                if self.inc < np.pi:
+                if np.degrees(self.inc) < 180:
                     tt_near[ix,iy] = np.abs(sol.x) # au
                     tt_far[ix,iy] = -np.abs(sol.x)
                 else:
                     tt_far[ix,iy] = np.abs(sol.x) # au
                     tt_near[ix,iy] = -np.abs(sol.x)
 
+        xx = xxp
         yy_near = yyp/np.cos(self.inc) + tt_near*np.sin(self.inc)
         zz_near = tt_near*np.cos(self.inc)
-        rr_near = np.sqrt(xx**2+yy_near**2+zz_near**2) * self.pix_to_au # au
-        rr_near_cm = rr_near*au # cm
+        rr_near_cm = np.sqrt(xx**2+yy_near**2+zz_near**2) * self.pix_to_au *au # cm
         theta_near = np.arctan2(yy_near,xx)
 
         yy_far = yyp/np.cos(self.inc) + tt_far*np.sin(self.inc)
         zz_far = tt_far*np.cos(self.inc)
-        rr_far = np.sqrt(xx**2+yy_far**2+zz_far**2) * self.pix_to_au # au
-        rr_far_cm = rr_far*au # cm
+        rr_far_cm = np.sqrt(xx**2+yy_far**2+zz_far**2) * self.pix_to_au*au # cm
         theta_far = np.arctan2(yy_far,xx)
 
         vel_near = np.sqrt(G* self.mstar/rr_near_cm)*np.sin(self.inc)*np.cos(theta_near)/1e5 # km/s
@@ -122,13 +120,13 @@ class Isovel:
         vel_near = rotate(vel_near,self.pa, reshape=False)
         vel_far = rotate(vel_far,self.pa, reshape=False)
 
-
-        mask_contour_near = rotate(rr_near, self.pa, reshape=False)[:][:]>self.border
-        mask_contour_far= rotate(rr_far, self.pa, reshape=False)[:][:]>self.border
-        mask_contour_thin = rotate(rr, self.pa, reshape=False)[:][:]>self.border
+        '''
+        mask_contour_near = rotate(rr_near_cm/au, self.pa, reshape=False)[:][:]>self.border
+        mask_contour_far= rotate(rr_far_cm/au, self.pa, reshape=False)[:][:]>self.border
+        mask_contour_thin = rotate(rr_cm/au, self.pa, reshape=False)[:][:]>self.border
         
         vel_near = np.ma.array(vel_near, mask=mask_contour_near)
         vel_far = np.ma.array(vel_far, mask=mask_contour_far)
         vel_thin = np.ma.array(vel_thin, mask=mask_contour_thin)
-
+        '''
         return vel_near, vel_far, vel_thin
